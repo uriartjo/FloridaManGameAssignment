8b5ee34d233e8a5c063c46076b8d80cbf5b79035
Joey Uriarte <Joey.Uriarte@saintandrews.net>
2021-08-19 09:57:10 -0400

    Snark Added

--- src/com/company/Headline.java
+++ src/com/company/Headline.java
@@ -3,25 +3,3 @@ package com.company;
 public class Headline {
 
 }
-
-/*
-Headline Class
-This class is responsible for holding the Florida Man story headlines.
-
-Required Class Attributes
-A string that will hold the headline's...well, story.
-A string, or keyword, that will be word being removed from the headline that the player to guess
-An ArrayList holding the string previously mentioned keyword alongside 3 more strings. These will be the "options" presented to the player to choose from
-
-Required Class Methods
-A constructor that takes in:
-The story
-The keyword
-The three additional choices
-Appropriate getters and setters
-A method that returns a String of the object's headline but with the keyword replaced by ______
-
-Other Requirements
-Upon being constructed, the headline object should automatically find itself placed into the app's ArrayList of existing Headlines. This ArrayList resides in the HeadlineBank class (see below for more).
-Upon being constructed, the Headline object should shuffle the elements in the ArrayList. This ensures the option order is always randomized.
- */
--- src/com/company/HeadlineBank.java
+++ src/com/company/HeadlineBank.java
@@ -4,18 +4,3 @@ public class HeadlineBank {
 
 }
 
-/*
-HeadlineBank Class
-This class is responsible for holding various headline objects.
-
-Required Class Attributes
-An ArrayList that will hold all Headline objects
-
-Required Class Methods
-A method to shuffle the elements (the headlines) within the ArrayList
-A method that will create, at a minimum, 10 Headline objects. You should not be adding them to the ArrayList in this method - only creating them.
-
-Notes:
-Think of this as the “live data” that will exist within your game - specifically that ArrayList
-The static keyword is your friend in this class.
- */
